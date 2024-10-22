4549c2cdd494575efe4f60e5695ccc6535378225
David Pages <david.pages01@estudiant.upf.edu>
2024-10-22 17:43:14 +0200

    First commit

--- src/antiprime.py
+++ src/antiprime.py
@@ -1,24 +1,46 @@
 ## ADD WHATEVER ARGUMENTS ARE NECESSARY TO THE MAIN FUNCTION
 ## IN THE SAME ORDER AS THE ARGUMENTS ARE TAKEN FROM THE
 ## COMMAND LINE SPECIFIED BELOW
-def main() :
+import sys
+def main(x) :
 	## YOU CODE SHOULD START HERE AST THE SAME
 	## IDENTATION AS THIS COMMENT
-
+	i = int (1)
+	e = int (0)
+	while (i <= x):
+		if (x % i == 0):
+			e = e + 1
+		i = i + 1
+	y = int (x - 1)
+	i = int (1)
+	f = int (0)
+	while (y >= 1 and e > f):
+		f = int (0)
+		i = int (1)
+		while (i <= y):
+			if (y % i == 0):
+				f = f + 1
+			i = i + 1
+		y = y - 1
+	if (f >= e):
+		res = ("not anti-prime")
+	else:
+		res = ("anti-prime")
 
 	## THE LAST LINES OF YOUR CODE SHOULD EITHER
 	## RETURN THE VALUE "anti-prime" or "not anti-prime"
 	## REPLACE THE FOLLOWING LINE BY WHATEVER LINES
 	## OF CODE ALLOW THIS FUNCTION TO RETURN THE VALUE
 	## "anti-prime" or "not anti-prime"
-	return("anti-prime")
+	return(res)
 
 ## DO NOT REMOVE THIS LINE BELOW
 if __name__ == "__main__" :
+	x = int(sys.argv[1])
 
 	## MODIFY THE LINE BELOW AND ADD BEFORE WHATEVER LINES ARE NECESSARY
 	## TO RUN THIS PROGRAM AS, FOR INSTANCE:
 	## $ python antiprime.py 6
 	## WHERE THE FIRST ARGUMENT IS A POSITIVE INTEGER NUMBER FOR WHICH
 	## YOU WANT TO FIGURE OUT WHETHER IS ANTI-PRIME OR NOT
-	print(main())
+	print(main(x))
